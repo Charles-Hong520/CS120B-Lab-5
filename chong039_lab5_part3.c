e49be89fc4ee85c528f575152f67f2bf714a0f6a
Charles-Hong520 <56359884+Charles-Hong520@users.noreply.github.com>
2021-10-17 12:01:52 -0700

    Update ex3

--- chong039_lab5_part3.c
+++ chong039_lab5_part3.c
@@ -1,17 +1,17 @@
-/*	Author: Charles Hong
+/*    Author: Charles Hong
  *  Partner(s) Name: 
- *	Lab Section:
- *	Assignment: Lab #5  Exercise #3
- *	Exercise Description: Cool Lights
- *	Link: 
- *	I acknowledge all content contained herein, excluding template or example
- *	code, is my own original work.
+ *    Lab Section:
+ *    Assignment: Lab #5  Exercise #3
+ *    Exercise Description: Cool Lights
+ *    Link: 
+ *    I acknowledge all content contained herein, excluding template or example
+ *    code, is my own original work.
  */
 #include <avr/io.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
-#define A (~PINA&0xFF)
+#define A (~PINA&0x01)
 enum State {start, on, off} state;
 unsigned char tmpA = 0x00;
 
@@ -20,6 +20,8 @@ int main(void) {
     DDRB = 0xFF; PORTB = 0x00; //output
     state = start;
     unsigned char cnt = 0;
+    unsigned char tempB = 0;
+    PORTA = PINA;
     while(1) {
         switch(state) {
             case start:
@@ -38,20 +40,21 @@ int main(void) {
             } else {
                 state = on;
                 cnt = (cnt+1)%11;
-                if(cnt==0) PORTB = 0x00;
-                else if(cnt==1) PORTB = 0x01;
-                else if(cnt==2) PORTB = 0x05;
-                else if(cnt==3) PORTB = 0x15;
-                else if(cnt==4) PORTB = 0x17;
-                else if(cnt==5) PORTB = 0x1F;
-                else if(cnt==6) PORTB = 0x3F;
-                else if(cnt==7) PORTB = 0x00;
-                else if(cnt==8) PORTB = 0x3F;
-                else if(cnt==9) PORTB = 0x00;
-                else if(cnt==10) PORTB = 0x3F;
+                if(cnt==0) tempB = 0x00;
+                else if(cnt==1) tempB = 0x01;
+                else if(cnt==2) tempB = 0x05;
+                else if(cnt==3) tempB = 0x15;
+                else if(cnt==4) tempB = 0x17;
+                else if(cnt==5) tempB = 0x1F;
+                else if(cnt==6) tempB = 0x3F;
+                else if(cnt==7) tempB = 0x00;
+                else if(cnt==8) tempB = 0x3F;
+                else if(cnt==9) tempB = 0x00;
+                else if(cnt==10) tempB = 0x3F;
              }
             break;
         }
+        PORTB=tempB;
     }
     return 1;
 }

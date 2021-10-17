d498f11613501ce608c95c7d83eab77dc5c0ac77
Charles-Hong520 <56359884+Charles-Hong520@users.noreply.github.com>
2021-10-17 12:41:11 -0700

    Add files via upload

--- chong039_lab5_part3.c
+++ chong039_lab5_part3.c
@@ -3,9 +3,10 @@
  *    Lab Section:
  *    Assignment: Lab #5  Exercise #3
  *    Exercise Description: Cool Lights
- *    Link: 
  *    I acknowledge all content contained herein, excluding template or example
  *    code, is my own original work.
+ *    Demo Link: https://drive.google.com/file/d/1UpYddyvRIJXJ2u1RK01PXOVTfbaOvnxp/view?usp=sharing
+ 
  */
 #include <avr/io.h>
 #ifdef _SIMULATE_
